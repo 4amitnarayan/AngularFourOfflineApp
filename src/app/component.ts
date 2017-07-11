44f29ac06be28f660e4a55bd6e487e167e3379d5
PRIYANKA KUMAR <priyankakumar@PRIYANKAs-MacBook-Pro.local>
2017-07-12 01:06:23 +0800

    first commit

---
+++ src/app/component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'student-app',
+  template: '<student-form></student-form>'
+})
+export class AppComponent {
+}
---
+++ src/app/student-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Student }    from './student';
+@Component({
+  selector: 'student-form',
+  templateUrl: './student-form.component.html'
+})
+export class StudentFormComponent {
+  courses = ['Computer', 'Electronics', 'Mechanical', 'Electrical','Chemical'];
+  model = new Student(308251, 'John Wrain Martin', this.courses[0], 'Engineering');
+  submitted = false;
+  onSubmit() {
+  		this.submitted = true;
+  }
+  active = true;
+  newStudent() {
+     this.model = new Student(0, '', '');
+     this.active = false;
+     setTimeout(() => this.active = true, 0);
+  }
+
+  default() {
+  	    this.model = new Student(308251, 'John Wrain Martin', this.courses[0], 'Engineering');
+  }
+  get diagnostic() {
+  		return JSON.stringify(this.model);
+  }
+}
